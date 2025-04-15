bc2ae2554ea8bb102cc831e36ddb6810a9ca989a
oaroua <aroua.oussema@gmail.com>
2025-04-15 10:41:15 +0100

    Initial commit

---
+++ app/src/main/java/dem/app/isims/MainActivity.kt
@@ -0,0 +1,18 @@
+package dem.app.isims
+
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+private const val TAG = "MainActivity"
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+}
